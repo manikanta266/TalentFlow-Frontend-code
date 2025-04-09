ec7b7e7a792fd2ce508f77a359db9582cd7daff5
MTL1021 <ravitejaganji49@gmail.com>
2025-04-09 18:36:13 +0530

    timesheet_changes

--- src/Timesheet/ManagerTimesheets.jsx
+++ src/Timesheet/ManagerTimesheets.jsx
@@ -4,6 +4,7 @@ import Loader from "./loader.js";
 import Calendar from "react-calendar";
 import { jsPDF } from "jspdf";
 import { MdOutlineFileDownload } from 'react-icons/md';
+import { FaHourglassHalf, FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import 'react-calendar/dist/Calendar.css';
 import {ChevronLeftIcon,ChevronRightIcon} from '@heroicons/react/20/solid';
 import "jspdf-autotable";
@@ -388,9 +389,12 @@ const ManagerTimesheets = () => {
                                 ? " text-green-500"
                                 : submission.status === "REJECTED"
                                 ? " text-red-500"
-                                : " text-blue-600"
+                                : " text-yellow-600"
                             }`}
                           >
+                             {submission.status === "APPROVED" && <FaCheckCircle className="mr-2" />}
+                             {submission.status === "REJECTED" && <FaTimesCircle className="mr-2" />}
+                             {submission.status === "PENDING" && <FaHourglassHalf className="mr-2" />}
                             {submission.status}
                           </span>
                         </td>
@@ -438,76 +442,6 @@ const ManagerTimesheets = () => {
             )}
  
             {/* Pagination */}
-            {/* <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-              <div className="flex-1 flex justify-between sm:hidden">
-                <button
-                  onClick={() => paginate(currentPage - 1)}
-                  disabled={currentPage === 1}
-                  className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                >
-                  Previous
-                </button>
-                <button
-                  onClick={() => paginate(currentPage + 1)}
-                  disabled={currentPage === totalPages}
-                  className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                >
-                  Next
-                </button>
-              </div>
-              <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                <div>
-                  <p className="text-lg text-gray-700">
-                    Showing{" "}
-                    <span className="font-medium">
-                      {indexOfFirstEmployee + 1}
-                    </span>{" "}
-                    to{" "}
-                    <span className="font-medium">
-                      {Math.min(indexOfLastEmployee, filteredSubmissions.length)}
-                    </span>{" "}
-                    of <span className="font-medium">{counts.total}</span>{" "}
-                    results
-                  </p>
-                </div>
-                <div>
-                  <nav
-                    className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-                    aria-label="Pagination"
-                  >
-                    <button
-                      onClick={() => paginate(currentPage - 1)}
-                      disabled={currentPage === 1}
-                      className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span className="sr-only">Previous</span>
-                      <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                    {[...Array(totalPages)].map((_, index) => (
-                      <button
-                        key={index}
-                        onClick={() => paginate(index + 1)}
-                        className={`relative inline-flex items-center px-4 py-2 border text-sm font-medium ${
-                          index + 1 === currentPage
-                            ? "z-10 bg-blue-50 border-blue-500 text-blue-600"
-                            : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50"
-                        }`}
-                      >
-                        {index + 1}
-                      </button>
-                    ))}
-                    <button
-                      onClick={() => paginate(currentPage + 1)}
-                      disabled={currentPage === totalPages}
-                      className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span className="sr-only">Next</span>
-                      <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                  </nav>
-                </div>
-              </div>
-            </div> */}
             <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
   <div className="flex-1 flex justify-between sm:hidden">
     <button
